53bf3b342998082b8581344c889b30dc0435b2a4
lawrencecchen <md.lawrence.chen@gmail.com>
2021-02-25 15:05:59 -0800

    animate-spin broken

--- example/rollup/src/App.svelte
+++ example/rollup/src/App.svelte
@@ -6,8 +6,9 @@
   <title>Svelte + Sveasy + Windicss</title>
 </svelte:head>
 
-<div class="flex flex-col items-center justify-center bg-indigo-900 min-w-100vw min-h-100vh">
+<div class="animate-spin flex flex-col items-center justify-center bg-indigo-900 min-w-100vw min-h-100vh">
   <Widget />
 </div>
 
-<style></style>
+<style>
+</style>
